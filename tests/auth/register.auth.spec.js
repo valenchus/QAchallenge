051bca18b8dd313d37a59da4e03d73b9fddb7181
ron2452 <matias.nahuel.pizzi@hotmail.com>
2024-01-17 09:36:54 -0300

    Refactor register page object and add new test cases

--- tests/auth/register.auth.spec.js
+++ tests/auth/register.auth.spec.js
@@ -5,9 +5,66 @@ import { RegisterPage } from "../../pageObjects/register.pageObject";
 
 test.describe("register user", () => {
 
-  test("register to the page", async ({ page }) => {
-    const registerPage = new RegisterPage(page);  
-    await registerPage.doRegister("mmm", "mmm", "m@m.com", "Asdasd*123", "Asdasd*123");
-    await registerPage.checkRegister();
+  // TC-0001
+  test("register with invalid email", async ({ page }) => {
+    const registerPage = new RegisterPage(page);
+    await registerPage.doRegister(
+      process.env.USER_NAME,
+      process.env.USER_USERNAME,
+      "invalidemail",
+      process.env.USER_PASSWORD,
+      process.env.USER_PASSWORD
+    );
+    await registerPage.checkEmailError();
+  });
+
+  test("register with invalid password", async ({ page }) => {
+    const registerPage = new RegisterPage(page);
+    await registerPage.doRegister(
+      process.env.USER_NAME,
+      process.env.USER_USERNAME,
+      process.env.USER_EMAIL,
+      "invalidpassword",
+      process.env.USER_PASSWORD
+    );
+    await registerPage.checkPasswordError();
+  });
+
+  test("register with invalid password confirmation", async ({ page }) => {
+    const registerPage = new RegisterPage(page);
+    await registerPage.doRegister(
+      process.env.USER_NAME,
+      process.env.USER_USERNAME,
+      process.env.USER_EMAIL,
+      process.env.USER_PASSWORD,
+      "invalidpassword"
+    );
+    await registerPage.checkConfirmPasswordError();
+  });
+
+  test("register with repeated username or email", async ({ page }) => {
+    const registerPage = new RegisterPage(page);
+    await registerPage.doRegister(
+      process.env.USER_NAME,
+      process.env.USER_USERNAME,
+      process.env.USER_EMAIL,
+      process.env.USER_PASSWORD,
+      process.env.USER_PASSWORD
+    );
+    await registerPage.doRegister(
+      process.env.USER_NAME,
+      process.env.USER_USERNAME,
+      process.env.USER_EMAIL,
+      process.env.USER_PASSWORD,
+      process.env.USER_PASSWORD
+    );
+    await registerPage.checkError();
+  })
+
+  // TC-0003
+  test("register with valid data", async ({ page }) => {
+    const registerPage = new RegisterPage(page);
+    await registerPage.doRegister("mmm", "mmm", "mmm", "mmm", "mmm");
+    await registerPage.checkError();
   });
 });
